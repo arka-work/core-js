9bbff95e8640c4139ed661a265e99b4071c9d624
arka-work <swdevintern2@chargeworks.in>
2025-04-25 01:31:40 +0530

    we kept some string method

---
+++ stringMethod.js
@@ -0,0 +1,33 @@
+
+{
+    const str="arka";
+    console.log(str.toUpperCase());
+}
+
+
+{
+    const str="HELLO";
+console.log(str.toLowerCase());
+}
+
+{
+    const str="seculrism!!!!!";
+    console.log(str.slice(0,6));
+}
+
+{
+    const str="this is arka";
+    console.log(str.replace("arka","JavaScript"));
+}
+
+{
+    const str="This is infrom you that , always belive on red";
+    console.log(str.charAt(2));
+}
+
+{
+    let str = "hello";
+
+    console.log(str.split(""));  
+
+}
